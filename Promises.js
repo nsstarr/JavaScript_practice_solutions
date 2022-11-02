7da770d76b52e7571ba3cf0cf9a4f2c70cd7edcd
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-11-02 11:29:44 +0000

    add a promise challenge solution to Promises.js

--- Promises.js
+++ Promises.js
@@ -85,17 +85,33 @@ const recordVideoThree = new Promise((resolve, reject) => {
   resolve("Video 3 Recorded");
 });
 
-Promise.all([
-    recordVideoOne, 
-    recordVideoTwo, 
-    recordVideoThree
-]).then((messages) => {
-    console.log(messages)
-})
-Promise.race([
-    recordVideoOne, 
-    recordVideoTwo, 
-    recordVideoThree
-]).then((message) => {
-    console.log(message)
-})
+Promise.all([recordVideoOne, recordVideoTwo, recordVideoThree]).then(
+  (messages) => {
+    console.log(messages);
+  }
+);
+Promise.race([recordVideoOne, recordVideoTwo, recordVideoThree]).then(
+  (message) => {
+    console.log(message);
+  }
+);
+
+// EXAMPLE 5
+
+function promiseAll(promises) {
+  const outputs = [];
+  let settledPromiseCounter = 0;
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise, i) => {
+      promise
+        .then((value) => {
+          outputs[i] = value;
+          settledPromiseCounter++;
+          if (settledPromiseCounter === promises.length) {
+            resolve(outputs);
+          }
+        })
+        .catch(reject);
+    });
+  });
+}

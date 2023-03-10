dcaf978e7244483f3acda6187d92f9ffcdabbe63
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-03-10 17:54:21 +0000

    add extactDomainName codewars solution

---
+++ codeWars/extractTheDomainName.js
@@ -0,0 +1,13 @@
+/*
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+*/
+
+function domainName(url){
+  let newStr = url.replace("http://", "").replace("https://", "").replace("www.", "")
+  
+  return newStr.split(".")[0]
+}
